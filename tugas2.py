fd30524d91a4d708be3af1da0e72212abd5263b1
prdpta <pradiptaputramei01@gmail.com>
2026-09-23 15:29:31 +0700

    menambahkan program pencarian angka terbesar dari tiga input

--- tugas2.py
+++ tugas2.py
@@ -1,12 +1,10 @@
 def cari_angka_terbesar():
     print("--- Program Mencari Angka Terbesar ---")
     try:
-        # Mengubah float() menjadi int() agar hasilnya berupa bilangan bulat
         a = int(input("Masukkan angka pertama: "))
         b = int(input("Masukkan angka kedua: "))
         c = int(input("Masukkan angka ketiga: "))
         
-        # Logika perbandingan
         if (a >= b) and (a >= c):
             terbesar = a
         elif (b >= a) and (b >= c):
